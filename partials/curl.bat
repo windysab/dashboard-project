5dfe6bd01ffd78f159c1790f6a9478c55d7b1e53
windysab <windysabtami85@gmail.com>
2024-10-22 11:39:22 +0800

    oek

---
+++ partials/curl.bat
@@ -0,0 +1,48 @@
+curl -o assets/css/style.css https://ptsp.pta-jakarta.go.id/files/css/style.css
+curl -o assets/css/utilities.css https://ptsp.pta-jakarta.go.id/files/css/utilities.css
+curl -o assets/css/custom.css https://ptsp.pta-jakarta.go.id/files/css/custom.css
+curl -o assets/css/fontawesome/all.min.css https://ptsp.pta-jakarta.go.id/files/css/fontawesome/all.min.css
+curl -o assets/css/themify-icons.css https://ptsp.pta-jakarta.go.id/files/css/themify-icons.css
+curl -o assets/css/animate.min.css https://ptsp.pta-jakarta.go.id/files/css/animate.min.css
+curl -o assets/css/vegas.min.css https://ptsp.pta-jakarta.go.id/files/css/vegas.min.css
+
+curl -o assets/js/jquery.min.js https://ptsp.pta-jakarta.go.id/files/js/jquery.min.js
+
+//<link rel="stylesheet" href="https://ptsp.pta-jakarta.go.id/files/css/bootstrap.min.css">
+
+curl -o assets/css/bootstrap.min.css https://ptsp.pta-jakarta.go.id/files/js/bootstrap.min.css
+
+
+-----------------------------------------------
+
+
+
+# Unduh jQuery
+curl -o assets/js/jquery.min.js https://code.jquery.com/jquery-3.6.0.min.js
+
+# Unduh Bootstrap Bundle
+curl -o assets/js/bootstrap.bundle.min.js https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js
+
+# Unduh jQuery Easing
+curl -o assets/js/jquery.easing.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js
+
+# Unduh jQuery Validate
+curl -o assets/js/jquery.validate.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js
+
+# Unduh jQuery Form
+curl -o assets/js/jquery.form.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js
+
+# Unduh jQuery Countdown
+curl -o assets/js/jquery.countdown.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery-countdown/2.2.0/jquery.countdown.min.js
+
+# Unduh Granim
+curl -o assets/js/granim.min.js https://cdnjs.cloudflare.com/ajax/libs/granim/2.0.0/granim.min.js
+
+# Unduh Vegas
+curl -o assets/js/vegas.min.js https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.js
+
+# Unduh jQuery.mb.YTPlayer
+curl -o assets/js/jquery.mb.YTPlayer.min.js https://cdnjs.cloudflare.com/ajax/libs/jquery.mb.YTPlayer/3.3.9/jquery.mb.YTPlayer.min.js
+
+# Unduh Modernizr
+curl -o assets/js/modernizr-custom.js https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js
