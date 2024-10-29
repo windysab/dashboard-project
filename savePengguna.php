d8ae27b1647c7d22724de8f1a7623dbb7e9fa71a
windysab <windysabtami85@gmail.com>
2024-10-29 14:12:37 +0800

    oke

--- savePengguna.php
+++ savePengguna.php
@@ -4,16 +4,17 @@ require 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$servername = getenv('DB_SERVER');
+$servername = getenv('DB_HOST');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
-$dbname = getenv('DB_NAME');
+$dbname = getenv('DB_DATABASE');
+$port = getenv('DB_PORT');
 
 // Debugging: Log environment variables (remove this in production)
-error_log("DB_SERVER: $servername, DB_USERNAME: $username, DB_NAME: $dbname");
+error_log("DB_HOST: $servername, DB_USERNAME: $username, DB_DATABASE: $dbname, DB_PORT: $port");
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
